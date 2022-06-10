878f25837ca172693536ca4cc15156c2a63cc018
Fede180600 <fede180600@libero.it>
2022-06-10 10:17:17 +0200

    use include

---
+++ partials/header.php
@@ -0,0 +1,28 @@
+<header>
+        <!-- header top -->
+        <div class="header-top">
+            <div class="header-top-left">
+                <span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" style="height: 24px; vertical-align: text-bottom;" alt=""></span>
+                <span style="line-height: 24px; color: #5f6368; opacity: 1; font-size: 22px; margin-left: 4px;">Privacy e termini</span>
+            </div>
+            <div class="header-top-right">
+                <span style="margin-right: 20px;"><i class="fas fa-ellipsis-v"></i><i class="fas fa-ellipsis-v"></i><i class="fas fa-ellipsis-v"></i></span>
+                <span><img src="img/user-img.jpg" alt="" style="height: 32px; border-radius: 50%; vertical-align: middle;"></span>
+            </div>
+        </div>
+        <!-- /header top -->
+
+        <!-- header bottom -->
+            <div class="header-bottom">
+                <nav class="header-navbar">
+                    <ul class="navbar-list">
+                        <li class="navbar-item">Introduzione</li>
+                        <li class="navbar-item">Norme sulla privacy</li>
+                        <li class="navbar-item">Termini di servizio</li>
+                        <li class="navbar-item">Tecnologie</li>
+                        <li class="navbar-item active">Domande frequenti</li>
+                    </ul>
+                </nav>
+            </div>
+        <!-- /header bottom -->
+    </header>
